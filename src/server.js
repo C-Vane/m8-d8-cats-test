30d76fe1b017e5ea3698c2941aada8ff9b7e9fb9
Vanessa Cattabiani <vanebrunocattabiani@gmail.com>
2021-02-26 11:23:10 +0100

    ok

--- src/server.js
+++ src/server.js
@@ -1,16 +1,16 @@
-require("dotenv").config()
+require("dotenv").config();
 
-const express = require("express")
-const server = new express()
+const express = require("express");
+const server = new express();
 
-const userRouter = require("./services/users")
+const userRouter = require("./services/users");
 
-server.use(express.json())
+server.use(express.json());
 
 server.get("/test", (req, res) => {
-    res.status(200).send({ message: "Test success" })
-})
+  res.status(200).send({ message: "Test success" });
+});
 
-server.use("/users", userRouter)
+server.use("/users", userRouter);
 
-module.exports = server
+module.exports = server;
